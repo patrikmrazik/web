ea92b086575cda202f0c287d80d79b1fd8862b05
patrikmrazik <48693789+patrikmrazik@users.noreply.github.com>
2019-03-18 20:18:13 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+<form action="index.php" method="post" enctype="multipart/form-data">
+	Jmeno: <input type="text" name="jmeno">
+	Prijmeni: <input type="text" name="prijmeni">
+	<input type="submit" value="Ulozit">
+</form>
+</body>
+</html>
